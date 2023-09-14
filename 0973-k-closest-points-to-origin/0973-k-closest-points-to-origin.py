eb1e67244546d3b12bc849562c155c34b48b43cc
Neurumaru <ksj6659@gmail.com>
2023-09-14 19:12:40 +0900

    973. K Closest Points to Origin - Runtime : 598ms (99.93%), Memory : 22.30MB (85.44%)

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -0,0 +1,5 @@
+from typing import List
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        return sorted(points, key=lambda x : x[0]*x[0] + x[1]*x[1])[:k]
